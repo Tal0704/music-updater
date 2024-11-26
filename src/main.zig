01f5064951c8d2f41f62cebdc92879576a5df2da
Tal Meron <talmeron0704@gmail.com>
2024-11-26 13:39:45 +0200

    organized structure and added a test for update

--- src/main.zig
+++ src/main.zig
@@ -5,15 +5,34 @@ const fs = std.fs;
 const cwd = fs.cwd();
 const ArrayList = std.ArrayList;
 const String = ArrayList(u8);
-const PageAllocator = std.heap.page_allocator;
 
 pub fn main() anyerror!void {
     var file: fs.File = try cwd.openFile("/home/tal/Documents/Obsidian Vault/music/music.md", .{ .mode = .read_only });
     defer file.close();
-    var url: String = String.init(PageAllocator);
+    try update(&file, std.heap.page_allocator);
+}
+
+fn update(file: *fs.File, allocator: std.mem.Allocator) !void {
+    var url: String = String.init(allocator);
+    var fileName: String = String.init(allocator);
     defer url.deinit();
-    var fileName: String = String.init(PageAllocator);
     defer fileName.deinit();
+
+    var downloaded = ArrayList([]const u8).init(allocator);
+    defer downloaded.deinit();
+
+    var musicFolder: fs.Dir = try cwd.openDir("/home/tal/Music", .{ .iterate = true });
+    defer musicFolder.close();
+
+    var walker = try musicFolder.walk(allocator);
+    defer walker.deinit();
+    while (try walker.next()) |f| {
+        try downloaded.append(f.basename[0 .. f.basename.len - 4]);
+    }
+
+    for (0..downloaded.items.len) |i| {
+        print("{s}\n", .{downloaded.items[i]});
+    }
 
     var buffReader = std.io.bufferedReader(file.reader());
     var inStream = buffReader.reader();
@@ -22,17 +41,21 @@ pub fn main() anyerror!void {
         const name = getName(line);
         const URL = getURL(line);
 
-        if (try isDownloaded(name)) {
+        // Check if is downloaded
+        if (try isDownloaded(name, allocator)) {
             print("{s} is already downloaded!\n", .{name});
         } else try downloadYT(URL, name);
+
+        // Delete if the file not in the music.md file
     }
 }
 
-fn isDownloaded(name: []const u8) !bool {
+fn isDownloaded(name: []const u8, allocator: std.mem.Allocator) !bool {
     var musicFolder: fs.Dir = try cwd.openDir("/home/tal/Music", .{ .iterate = true });
     defer musicFolder.close();
 
-    var walker = try musicFolder.walk(PageAllocator);
+    var walker = try musicFolder.walk(allocator);
+    defer walker.deinit();
     while (try walker.next()) |file| {
         if (std.mem.eql(u8, name, file.basename[0 .. file.basename.len - 4]))
             return true;
@@ -100,5 +123,12 @@ test "checkIfDownloaded" {
     const line = "[Metallica - Master of Puppets](https://www.youtube.com/watch?v=mOOzcgDLCbQ&list=WL&index=58)";
     const name = getName(line);
 
-    try expect(try isDownloaded(name));
+    try expect(try isDownloaded(name, std.testing.allocator));
+}
+
+test "update()" {
+    var file: fs.File = try cwd.openFile("/home/tal/Documents/Obsidian Vault/music/music.md", .{ .mode = .read_only });
+    defer file.close();
+
+    try update(&file, std.testing.allocator);
 }
