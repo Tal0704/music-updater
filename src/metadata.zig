162305ccd34510f0632e3a4563b6f4ba8c3dc40e
Tal Meron <talmeron0704@gmail.com>
2024-12-13 10:49:42 +0200

    Change to optional on every field of Metada

--- src/metadata.zig
+++ src/metadata.zig
@@ -3,13 +3,16 @@ const ArrayList = std.ArrayList;
 const String = ArrayList(u8);
 
 pub const metadata = struct {
-    album: String,
-    artist: String,
-    year: String,
+    album: String.?,
+    artist: String.?,
+    year: String.?,
 
     pub fn deinit(self: @This()) void {
-        self.artist.deinit();
-        self.album.deinit();
-        self.year.deinit();
+        if (self.artist) |art|
+            art.deinit();
+        if (self.album) |al|
+            al.deinit();
+        if (self.year) |y|
+            y.deinit();
     }
 };
