dd55ebf06fbc98d0abcd0d86dd0635e4e6e77ac6
Tal Meron <talmeron0704@gmail.com>
2024-12-13 11:03:13 +0200

    Add songName field to metadata

--- src/metadata.zig
+++ src/metadata.zig
@@ -3,11 +3,13 @@ const ArrayList = std.ArrayList;
 const String = ArrayList(u8);
 
 pub const metadata = struct {
+    songName: String,
     album: String.?,
     artist: String.?,
     year: String.?,
 
     pub fn deinit(self: @This()) void {
+        self.songName.deinit();
         if (self.artist) |art|
             art.deinit();
         if (self.album) |al|
