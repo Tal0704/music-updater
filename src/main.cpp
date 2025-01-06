3acdecaa22b3457d6289be54c77cc86ce10df17c
Tal Meron <talmeron0704@gmail.com>
2025-01-06 12:53:14 +0200

    removed download test

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,6 @@
 namespace fs = std::filesystem;
 using json = nlohmann::json;
 
-void testingDownload() {
-	Album::Ptr a = std::make_unique<Album>();
-	Song s("orion", a.get());
-	/* s.album->year = "1986"; */
-	/* s.album->artist = "Metallica"; */
-	/* s.album->name = "Master of Puppets"; */
-	/* s.album->imageURL = "https://i.scdn.co/image/ab67616d0000b2731a84d71391df7469c5ab8539"; */
-	s.URL = "https://www.youtube.com/watch?v=E0ozmU9cJDg";
-	s.download("/home/tal/Music/t");
-}
-
 void testingunneeded() {
 	std::ifstream libFile("/home/tal/Documents/Obsidian Vault/music/music.md");
 	fs::path musicPath = "/home/tal/Music";
